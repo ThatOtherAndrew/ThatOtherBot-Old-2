be82651acd7118261653e61bd080fa8d8b34d065
Andrew <stroev.andrew@gmail.com>
2022-03-12 18:29:18 +0000

    💨 Subclass `Bot` and move logger

--- main.py
+++ main.py
@@ -5,27 +5,29 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.INFO)
-logger_file_handler = logging.FileHandler('logs/discord.log', 'w', 'utf-8')
-logger_file_handler.setFormatter(logging.Formatter('[%(asctime)s %(levelname)s] %(name)s: %(message)s'))
-logger_stream_handler = logging.StreamHandler(sys.stdout)
-logger_stream_handler.setFormatter(logging.Formatter('[%(levelname)s] %(name)s: %(message)s'))
-logger.addHandler(logger_file_handler)
-logger.addHandler(logging.StreamHandler(sys.stdout))
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+class Bot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.logger = logging.getLogger('discord')
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('[%(asctime)s %(levelname)s] %(name)s: %(message)s')
+        file_handler = logging.FileHandler('logs/discord.log', 'w', 'utf-8')
+        file_handler.setFormatter(formatter)
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+bot = Bot(command_prefix='!', intents=discord.Intents.all())
 tree = app_commands.CommandTree(bot)
 
 
 @bot.event
 async def on_ready():
-    logger.info(f'Logged in as {bot.user}')
-
-
-@bot.command()
-async def test(ctx: commands.Context):
-    await ctx.reply('Received!')
+    bot.logger.info(f'Logged in as {bot.user}')
 
 
 if __name__ == '__main__':
