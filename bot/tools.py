3e9399c4a0ec110bb18725058b26ad9562c1068b
Andrew <stroev.andrew@gmail.com>
2022-03-21 20:30:13 +0000

    🪵 Refactor out logging init, add docstrings

--- bot/tools.py
+++ bot/tools.py
@@ -1,7 +1,23 @@
+import logging
 from pathlib import Path
 
 
+def init_logger() -> logging.Logger:
+    """Initialise the logger and return an instance"""
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.INFO)
+    logging_formatter = logging.Formatter('[%(asctime)s %(levelname)s] %(name)s: %(message)s')
+    handler = logging.FileHandler('logs/discord.log', 'w', 'utf-8')
+    handler.setFormatter(logging_formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+log = init_logger()
+
+
 def get_extensions(search_dir: str) -> list[str]:
+    """Recursively gets the dot-separated import syntax of all Python files in a directory"""
     search_path = Path(search_dir)
     if not search_path.is_dir():
         raise FileNotFoundError(f'{search_dir} is not a valid directory.')
--- main.py
+++ main.py
@@ -1,34 +1,28 @@
-import logging
-
 import discord
 from discord.ext import commands
 
 from bot import settings
-from bot.tools import get_extensions
+from bot.tools import log, get_extensions
 
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
-        self.logger = logging.getLogger('discord')
-        self.logger.setLevel(logging.INFO)
-        formatter = logging.Formatter('[%(asctime)s %(levelname)s] %(name)s: %(message)s')
-        handler = logging.FileHandler('logs/discord.log', 'w', 'utf-8')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        self.settings = settings.load('settings.toml')
 
         super().__init__(*args, **kwargs)
-
-        self.settings = settings.load('settings.toml')
 
     async def setup_hook(self):
         for extension in get_extensions('bot/extensions'):
             try:
                 await bot.load_extension(extension)
-                bot.logger.info(f'Extension loaded: {extension}')
+                log.info(f'Extension loaded: {extension}')
             except Exception as extension_error:
-                bot.logger.error(extension_error)
+                log.error(extension_error)
 
-        bot.logger.info(f'Logged in as {bot.user}')
+        log.info(f'Logged in as {bot.user}')
+
+    async def add_cog(self, cog: commands.Cog, /, *, settings_template: dict[str, dict] = None, **kwargs):
+        await super().add_cog(cog, **kwargs)
 
 
 bot = Bot(command_prefix='!', intents=discord.Intents.all())
@@ -36,7 +30,7 @@ bot = Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.tree.command()
 async def test(interaction: discord.Interaction):
-    await interaction.response.send_message('Received!', ephemeral=True)
+    await interaction.response.send_message('Received!')
 
 
 if __name__ == '__main__':
