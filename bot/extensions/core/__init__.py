80283a1c9e096723b1308c3f056cf11017e45804
Andrew <stroev.andrew@gmail.com>
2022-04-11 13:22:31 +0100

    💻 Add basic eval command

--- bot/extensions/core/__init__.py
+++ bot/extensions/core/__init__.py
@@ -18,6 +18,15 @@ class Core(commands.Cog):
     def __init__(self, bot: 'Bot'):
         self.bot = bot
 
+    @commands.command(aliases=['eval'])
+    @commands.is_owner()
+    async def evaluate(self, ctx: commands.Context, *, expression: str = None):
+        if expression is None:
+            await ctx.send_help()
+            return
+
+        await ctx.reply(eval(expression, {'bot': self.bot, 'ctx': ctx}))
+
     @commands.command()
     async def sync(self, ctx: commands.Context):
         await self.bot.tree.sync(guild=discord.Object(id=ctx.guild.id))
